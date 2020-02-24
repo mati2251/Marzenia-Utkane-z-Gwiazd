ae0bbe59ac0353f9c306eb36c69ee28fb417bf62
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2020-02-24 20:49:42 +0100

    Add creators

--- src/pages/organizatorzy.js
+++ src/pages/organizatorzy.js
@@ -3,16 +3,43 @@ import Layout from "../components/UI/Layout/Layout"
 import SponsorSlider from "../components/SponsorsSlider/SponsorsSlider"
 import styles from "../styles/pages.module.scss"
 import Creator from "../components/Creators/Creator/Creator";
+import {graphql} from "gatsby";
+import stylesFromArtist from "../components/Artists/Artisits.module.scss"
 
-const organizatorzy = () => {
+const organizatorzy = ({data}) => {
+
+    const creatorsJSX = data.data.organizatorzies.map((item) => {
+        return( <Creator name={item.imie} description={item.opis} image={item.zdjecie.url}/> )
+    });
+
+    console.log(creatorsJSX);
+
     return (
       <Layout>
+          <div className={stylesFromArtist.Artists__Container}>
+              {creatorsJSX}
+
+          </div>
           <div className={styles.indexMain}>
-              <Creator name="Mateusz Karłowski" description="Uczeń Technikum Informatycznego twórca stronny internetowej"/>
               <SponsorSlider/>
           </div>
       </Layout>
     )
 };
+
+export const query = graphql`
+    query {
+        data {
+            organizatorzies {
+                imie
+                opis
+                zdjecie {
+                    url
+                }
+            }
+        }
+    }
+
+`;
 
 export default organizatorzy
