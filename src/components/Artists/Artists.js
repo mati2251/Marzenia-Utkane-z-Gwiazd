37fb08036d6d20fc7d27690746412617b2bcb638
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2020-04-19 16:07:15 +0200

    Update defult artist year

--- src/components/Artists/Artists.js
+++ src/components/Artists/Artists.js
@@ -7,73 +7,71 @@ import Artist from "./Artist/Artist"
 import SponsorSlider from "../SponsorsSlider/SponsorsSlider"
 
 const Artists = props => {
-  const query = useStaticQuery(graphql`
-    query {
-      data {
-        roks {
-          artyscis {
-            nazwa
-            opis
-            id
-            link
-            zdjecie {
-              url
+    const query = useStaticQuery(graphql`
+        query {
+            data {
+                roks {
+                    artyscis {
+                        nazwa
+                        opis
+                        id
+                        link
+                        zdjecie {
+                            url
+                        }
+                    }
+                    rok
+                }
             }
-          }
-          rok
         }
-      }
-    }
-  `)
+    `)
 
-  const yearsJSX = query.data.roks.map(item => {
-    return (
-      <option value={item.rok} key={item.rok}>
-        {item.rok}
-      </option>
-    )
-  })
-
-  const prop = props.year.split("=")
-  let year = prop[1]
-  if (props.year === "") {
-    year = Math.max(
-      ...query.data.roks.map(item => {
-        return item.rok
-      })
-    ).toString()
-  }
-
-  const artistsJSX = query.data.roks
-    .filter(item => item.rok === year)[0]
-    .artyscis.map(item => {
-      return (
-        <Artist
-          name={item.nazwa}
-          key={item.id}
-          description={item.opis}
-          link={item.link}
-          image={item.zdjecie.url}
-        />
-      )
+    const yearsJSX = query.data.roks.map(item => {
+        return (
+          <option value={item.rok} key={item.rok}>
+              {item.rok}
+          </option>
+        )
     })
 
-  return (
-    <div className={styles.container}>
-      <h2 className={styles.Artist__labelContainer}>
-        Edycja Koncertu
-        <select
-          onChange={value => navigate(`/artysci?rok=${value.target.value}`)}
-        >
-          {yearsJSX}
-        </select>
-      </h2>
-      <div className={styles.Artists__Container}>{artistsJSX}</div>
-      <div className={style.indexMain}>
-        <SponsorSlider />
+    const prop = props.year.split("=")
+    let year = prop[1]
+    if (props.year === "") {
+        year = query.data.roks[query.data.roks.length-1].rok;
+    }
+
+    console.log(year)
+
+    const artistsJSX = query.data.roks
+      .filter(item => item.rok === year)[0]
+      .artyscis.map(item => {
+          return (
+            <Artist
+              name={item.nazwa}
+              key={item.id}
+              description={item.opis}
+              link={item.link}
+              image={item.zdjecie.url}
+            />
+          )
+      })
+
+    return (
+      <div className={styles.container}>
+          <h2 className={styles.Artist__labelContainer}>
+              Edycja Koncertu
+              <select
+                onChange={value => navigate(`/artysci?rok=${value.target.value}`)}
+              >
+                  {yearsJSX}
+              </select>
+          </h2>
+          <div className={styles.Artists__Container}>{artistsJSX}</div>
+          <div className={style.indexMain}>
+              <SponsorSlider/>
+          </div>
       </div>
-    </div>
-  )
+    )
 }
 
 export default Artists
