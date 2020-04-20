a3669d4aae6e2838360cd90fdf91d880ccb945e8
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2020-04-20 22:23:47 +0200

    Fix artist bug with artist without link and description artist can scroll

--- src/components/Artists/Artist/Artist.js
+++ src/components/Artists/Artist/Artist.js
@@ -2,19 +2,20 @@ import React from "react"
 import style from "./Artist.module.scss"
 
 const Artist = props => {
-  return (
-    <div className={style.artist}>
-      <img src={props.image} alt={props.name} />
-      <div className={style.artist__nameDescription}>
-        <h2>{props.name}</h2>
-        <h3>{props.description}</h3>
-        <a href={props.link}>
-          <h3> Wiecej o mnie </h3>
-        </a>
-      </div>
-      <div className={style.artistBlackBorder} />
-    </div>
-  )
+	return (
+		<div className={style.artist}>
+			<img src={props.image} alt={props.name}/>
+			<div className={style.artist__nameDescription}>
+				<h2>{props.name}</h2>
+				<h3 className={style.artist__description}>{props.description}</h3>
+				{props.link !== null ?
+					<a href={props.link}>
+						<h3> Wiecej o mnie </h3>
+					</a> : null}
+			</div>
+			<div className={style.artistBlackBorder}/>
+		</div>
+	)
 }
 
 export default Artist
