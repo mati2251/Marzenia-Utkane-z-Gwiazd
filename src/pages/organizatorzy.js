36b2acd0174705f7d22a0b91fda57bb01a309d37
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2020-04-28 11:04:58 +0200

    Divide creators to head creators and other

--- src/pages/organizatorzy.js
+++ src/pages/organizatorzy.js
@@ -3,43 +3,67 @@ import Layout from "../components/UI/Layout/Layout"
 import SponsorSlider from "../components/SponsorsSlider/SponsorsSlider"
 import styles from "../styles/pages.module.scss"
 import Creator from "../components/Creators/Creator/Creator"
-import { graphql } from "gatsby"
+import {graphql} from "gatsby"
 import stylesFromArtist from "../components/Artists/Artisits.module.scss"
 
-const organizatorzy = ({ data }) => {
-  const creatorsJSX = data.data.organizatorzies.map(item => {
-    return (
-      <Creator
-        name={item.imie}
-        description={item.opis}
-        image={item.zdjecie.url}
-      />
-    )
-  })
+const organizatorzy = ({data}) => {
+
+	const headCreatorsJSX = data.data.organizatorzies.filter(item => item.zalozyciel === true).map(item => {
+		return (
+			<Creator
+				key={item.id}
+				name={item.imie}
+				description={item.opis}
+				image={item.zdjecie.url}
+			/>
+		)
+	})
+
+	const creatorsJSX = data.data.organizatorzies.filter(item => item.zalozyciel === false).map(item => {
+		return (
+			<Creator
+				key={item.id}
+				name={item.imie}
+				description={item.opis}
+				image={item.zdjecie.url}
+			/>
+		)
+	})
 
 
-  return (
-    <Layout>
-      <div className={stylesFromArtist.Artists__Container}>{creatorsJSX}</div>
-      <div className={styles.indexMain}>
-        <SponsorSlider />
-      </div>
-    </Layout>
-  )
+	return (
+		<Layout>
+			<div>
+				<h1>Organizatorzy założyciele</h1>
+				<div className={stylesFromArtist.Artists__Container}>
+				{headCreatorsJSX}
+				</div>
+				<h1>Pozostali Organizatorzy</h1>
+				<div className={stylesFromArtist.Artists__Container}>
+				{creatorsJSX}
+				</div>
+			</div>
+			<div className={styles.indexMain}>
+				<SponsorSlider/>
+			</div>
+		</Layout>
+	)
 }
 
 export const query = graphql`
-  query {
-    data {
-      organizatorzies {
-        imie
-        opis
-        zdjecie {
-          url
+    query {
+        data {
+            organizatorzies {
+                id
+                imie
+                opis
+                zalozyciel
+                zdjecie {
+                    url
+                }
+            }
         }
-      }
     }
-  }
 `
 
 export default organizatorzy
