6f054627a8b783ca7c59cc8b7c78f985dfa0c1ee
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2020-05-08 22:33:45 +0200

    Add dynamic generate concerts

--- src/pages/koncerty.js
+++ src/pages/koncerty.js
@@ -2,15 +2,25 @@ import React from "react";
 import Layout from "../components/UI/Layout/Layout";
 import {graphql} from "gatsby";
 import ConcertLink from "../components/ConcertLink/ConcertLink";
+import styles from "../styles/pages.module.scss";
+import SponsorSlider from "../components/SponsorsSlider/SponsorsSlider";
 
 const koncerty = ({data}) => {
-	const concertsJSX = {}
+	const concertsJSX = data.data.roks.map((item) => {
+		return <ConcertLink value={item.value} image={item.zdjecieGlowne.url} name={item.rok} key={item.value}/>
+	})
+
 
 	return (
 		<Layout>
 			<h1>Koncerty</h1>
-			<h2 style={{"fontStyle": "italic"}}>Zobacz nasze koncerty i ich osiągniecia <span role="img">🎉</span> !</h2>
-			<div>
+			<h2 style={{"fontStyle": "italic"}}>Zobacz nasze koncerty i ich osiągniecia <span role="img">🎉</span> !
+			</h2>
+			<div className={styles.concerts__container}>
+				{concertsJSX.reverse()}
+			</div>
+			<div className={styles.indexMain}>
+				<SponsorSlider/>
 			</div>
 		</Layout>
 	)
